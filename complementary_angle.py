10130526dc35f936726c5454c16c9226663a3f45
DEEPAK SOMASUNDARAM <69519376+Dek1024@users.noreply.github.com>
2023-03-23 00:28:21 +0530

    Update complementary_angle.py

--- complementary_angle.py
+++ complementary_angle.py
@@ -1,7 +1,16 @@
-#Getting a comac complelemtray angle for a given number
-a = input("Enter an anglr between 0 to 90 degrees")
-a = float(a)
-if (a <0 or a > 90):
-    print("Invalid number")
+#Getting a complelemtray angle for the given angle
+def main():
+    a = input("Enter an anglr between 0 to 90 degrees")
+    a = float(a)
+    if (a <0 or a > 90):
+        print("Invalid number")
+        exit()
+    print("The complementary angle is", 90 - a)
+
+restart = print("Do you want to test the code again (answer by typing yes or no) ?").lower()
+if restart == "yes":
+    main()
+else:
     exit()
-print("The complementary angle is", 90 - a)
+   
+main()
