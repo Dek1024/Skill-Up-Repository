1f37b714e2ba6b66086febdd03e95819a92e322c
DEEPAK SOMASUNDARAM <69519376+Dek1024@users.noreply.github.com>
2023-11-28 16:22:28 +0530

    Add files via upload

---
+++ neg_index_array.py
@@ -0,0 +1,7 @@
+checkString = "Fight the Power"
+if checkString[-4] == "o":
+    print("Actual result:", checkString[-4])
+    print("Expected result:", "o (as in po)")
+    print("Pass")
+else:
+    print("Fail")
